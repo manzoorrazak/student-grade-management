899afbef94d8dcf424f0cbb3229701d0fe842fe7
manzoorrazak <manzoor.razak3@gmail.com>
2026-06-28 00:43:13 +0400

    Remove build artifacts

--- StudentGrades.Api/obj/Debug/net9.0/StudentGrades.Api.AssemblyInfo.cs
+++ StudentGrades.Api/obj/Debug/net9.0/StudentGrades.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentGrades.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6537d4cd237ede852683832b7c5b7a27f168996")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd78ca3e80f0c9887270ea607b291f1a6ecd7488")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentGrades.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentGrades.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
